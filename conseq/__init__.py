362473d93441df1bbb4a4c13a5546ae0ea569ee4
Philip Montgomery <pmontgom@broadinstitute.org>
2020-02-14 12:48:03 -0500

    dropped paramiko dependency because ssh support is broken/disabled anyway

--- conseq/__init__.py
+++ conseq/__init__.py
@@ -1,6 +1,6 @@
 import json
 
-__version__ = '1.15.2'
+__version__ = '1.15.3'
 
 
 def Local(name):
--- conseq/xref.py
+++ conseq/xref.py
@@ -67,33 +67,12 @@ class Pull:
         self.ssh_client_cache = {}
         self.config = config
 
-    def _get_ssh_client(self, host):
-        client = paramiko.SSHClient()
-        # client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
-
-        sshconfig = paramiko.config.SSHConfig()
-        config_name = os.path.expanduser("~/.ssh/config")
-        sshconfig.parse(open(config_name))
-        host_config = sshconfig.lookup(host)
-        if host_config != None:
-            client.connect(host_config["hostname"], username=host_config['user'],
-                           key_filename=host_config['identityfile'])
-        else:
-            client.connect(host)
-
-        self.ssh_client_cache[host] = client
-        return client
-
     def pull(self, url: str, dest_path: str) -> str:
         log.warning("Downloading {} -> {}".format(url, dest_path))
 
         parts = urlparse(url)
         if parts.scheme == "ssh":
-            client = self._get_ssh_client(parts.netloc)
-            transport = client.get_transport()
-            sftp = transport.open_sftp_client()
-            sftp.get(parts.path, dest_path)
+            raise Exception("ssh no longer supported")
         elif parts.scheme in ["s3"]:
             s3_fetch(parts.netloc, parts.path, dest_path, self.config)
         elif parts.scheme in ["http", "https"]:
